48018f9dcb058f1541e6db9fdd1567f1b8ed374e
maarjaroots <maarja.roots@gmail.com>
2021-06-12 12:09:31 +0100

    building webpack

--- recipes-app/src/index.js
+++ recipes-app/src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react";  //Babel will convert import into require('module/path'), require function is how CommonJS modules are typically loaded
 import {render} from "react-dom";
 import Menu from "./components/Menu";
 import data from "./data/recipes.json"
 
 
-render(<Menu recipes={data} />, document.getElementById("root"))
+render(<Menu recipes={data} />, document.getElementById("root"));
