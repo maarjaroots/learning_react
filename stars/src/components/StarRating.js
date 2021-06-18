f30b37bb73e7158a178f4a03cf135afa206e4ebf
maarjaroots <maarja.roots@gmail.com>
2021-06-18 08:09:16 +0100

    component for the stars

---
+++ stars/src/components/StarRating.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating() {
+  return (
+    
+      const Star = ({selected = false}) => (
+      <FaStar color={selected ? "red" : "grey"} />
+      );
+    
+  );
+}
+
+{
+  /* <FaStar color="red" />
+        <FaStar color="red" />
+        <FaStar color="red" />
+        <FaStar color="grey" />
+        <FaStar color="grey" /> */
+}
